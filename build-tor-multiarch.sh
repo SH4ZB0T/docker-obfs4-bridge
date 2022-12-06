3e1ed81590f9bd3a9d0e142a2184737784c58a87
Wayne Reynolds <war@infodox.net>
2022-12-06 14:12:20 -0600

    Tor devs moving from gitolite to gitlab

--- build-tor-multiarch.sh
+++ build-tor-multiarch.sh
@@ -7,7 +7,7 @@ git_tag="$2"
 if [[ "$arch" == "arm64" ]]; then arch="aarch64"; fi
 if [[ "$arch" == "amd64" ]]; then arch="x86-64"; fi
 
-git clone -b ${git_tag} --single-branch https://git.torproject.org/tor.git
+git clone -b ${git_tag} --single-branch https://gitlab.torproject.org/tpo/core/tor.git
 
 cd tor/
 
